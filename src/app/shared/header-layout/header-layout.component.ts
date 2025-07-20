71f6795a73e7e873d6bf57fae7526d5b60cb6eef
Mai Phi Hieu <mphieu2003@gmail.com>
2025-07-20 22:09:48 +0700

    update angular

---
+++ src/app/shared/header-layout/header-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+    selector:'header-layout',
+    templateUrl:'./header-layout.component.html',
+    styleUrl: './header-layout.component.css'
+})
+
+export class HeaderLayoutComponent{
+    
+}
